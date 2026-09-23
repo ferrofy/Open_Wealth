5008485db3e2128058e9949d662a6289f0dc076c
FerroFy <team.ferrofy@gmail.com>
2026-09-23 17:27:15 +0530

    Balance Update

--- JS.js
+++ JS.js
@@ -6,8 +6,8 @@ const Asset_List_BTC = document.getElementById("Asset_List_BTC")
 const Asset_List_Gold = document.getElementById("Asset_List_Gold")
 const Assets_Total = document.getElementById("Assets_Total")
 
-BTC_Bal = { GoSats: 1120 }
-const Gold_Bal = { GoSats: 4.57, Paytm: 0 }
+BTC_Bal = { GoSats: 1258 }
+const Gold_Bal = { GoSats: 5.92, Paytm: 0.3 }
 
 let Total_BTC = 0
 let Total_Gold = 0
